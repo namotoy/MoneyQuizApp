79978c5e3694aae1737f3723e68776e725c28f5e
namotoy <namotopet0615@gmail.com>
2022-05-13 22:19:16 +0900

    問題の追加と変更

--- MoneyQuizApp/ScoreViewController.swift
+++ MoneyQuizApp/ScoreViewController.swift
@@ -11,10 +11,10 @@ class ScoreViewController: UIViewController {
     
     @IBOutlet weak var scoreLabel: UILabel!
     var correct = 0
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         scoreLabel.text = "\(correct)問正解!"
         // Do any additional setup after loading the view.
     }
@@ -24,13 +24,13 @@ class ScoreViewController: UIViewController {
     }
     
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
